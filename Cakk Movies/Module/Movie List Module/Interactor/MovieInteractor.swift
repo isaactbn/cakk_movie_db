6f72118c4ac1555c957b0a2b02e9bbe037efe05f
Isaac Newton Tambunan <isaac.tambunan@ottodigital.id>
2023-01-18 02:30:50 +0700

    Implement Movie List

---
+++ Cakk Movies/Module/Movie List Module/Interactor/MovieInteractor.swift
@@ -0,0 +1,29 @@
+//
+//  MovieInteractor.swift
+//  Cakk Movies
+//
+//  Created by Isaac on 01/18/23.
+//
+
+import Foundation
+
+protocol MovieInteractor {
+    var presenter: MoviePresenter? { get set }
+    
+    func getMovieList(id: String, page: Int)
+}
+
+class MovieInteractors: MovieInteractor {
+    var presenter: MoviePresenter?
+    
+    func getMovieList(id: String, page: Int) {
+        let repo = CARequestService<MovieBodyResponse>()
+        let path = page == 0 ? "&with_genres=\(id)" : "&with_genres=\(id)&page=\(page)"
+        repo.request(api: "/discover/movie", path: path, onSuccess: { (response) in
+            self.presenter?.onGetMovieList(with: .success(response))
+        }) { (error) in
+            self.presenter?.onGetMovieList(with: .failure(error))
+        }
+    }
+}
+     
